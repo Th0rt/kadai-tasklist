2343cbdf8841b309f0d644d88a511aaa73c2b949
Thort <yamaxanado@gmail.com>
2017-09-01 02:53:16 +0000

    fix:タスク編集
    登録ユーザー以外がタスクを編集できた問題を修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,8 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, only: [:show, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
+  
   
   def index
     @tasks = current_user.tasks.page(params[:page])
